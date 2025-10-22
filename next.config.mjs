0dfeb1aac1baaa8ba162c5e48d9281234bf07eab
Danzel Serrano <danzel.serrano@gmail.com>
2025-10-21 20:17:54 -0400

    updated so blog section renders latex with katex

---
+++ next.config.mjs
@@ -0,0 +1,18 @@
+import createMDX from "@next/mdx";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+};
+
+const withMDX = createMDX({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
+});
+
+export default withMDX(nextConfig);
